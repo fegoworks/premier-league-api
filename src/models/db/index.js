9f43addb9a45fe767ed891bc602f8c32e65d6962
unknown <fegoworks@hotmail.com>
2020-01-27 16:46:40 +0100

    chore(database): configure database connection

---
+++ src/models/db/index.js
@@ -0,0 +1,40 @@
+import {
+  Pool
+} from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+let DB = '';
+
+if (process.env.NODE_ENV === 'TEST') {
+  DB = process.env.TEST_URL;
+} else {
+  DB = process.env.DB_URL;
+}
+const pool = new Pool({
+  connectionString: DB,
+});
+
+pool.on('connect', () => {
+  console.log('connected to the database');
+});
+
+
+/**
+ * DB Query
+ * @param {object} req
+ * @param {object} res
+ * @returns {object} object
+ */
+const query = (text, params) => new Promise((resolve, reject) => {
+  pool.query(text, params)
+    .then((res) => {
+      resolve(res);
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
+
+export default query
