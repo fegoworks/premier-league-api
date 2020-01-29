8b3ab3bdea51d7379aeb815c2a30489c9454ec31
unknown <fegoworks@hotmail.com>
2020-01-29 11:59:34 +0100

    feat(user-sign-in): create controller logic for user sign in

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,6 @@
 import UserModel from '../models/user.model';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 class UserController {
   /**
@@ -28,6 +30,59 @@ class UserController {
       return res.status(400).json({
         status: 'Request failed',
         error: error,
+      });
+    }
+  }
+
+  static async signIn(req, res) {
+    try {
+      const { email, password } = req.body;
+      const { rows, rowCount } = await UserModel.findByEmail(email);
+
+      if (rowCount < 1) {
+        return res.status(401).json({
+          error: 'User not found',
+        });
+      }
+
+      const { ...data } = rows[0];
+
+      // Compare password with what's stored in the database
+      const isMatch = bcrypt.compareSync(password, data.password);
+      if (!isMatch) {
+        return res.status(401).json({
+          status: 'Request failed',
+          error: 'Wrong Password',
+        });
+      }
+
+      // Generate token
+      const token = jwt.sign(
+        {
+          id: data.userid,
+          isAdmin: data.isAdmin,
+        },
+        process.env.SECRET,
+        {
+          expiresIn: 86400, // expires in 24 hours
+        }
+      );
+
+      return res.status(200).json({
+        status: 'success',
+        message: `Welcome ${data.firstname}`,
+        data: {
+          token,
+          userId: data.userid,
+          firstName: data.firstname,
+          lastName: data.lastname,
+          email: data.email,
+        },
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: 'Request failed',
+        error: 'Wrong Credentials',
       });
     }
   }
