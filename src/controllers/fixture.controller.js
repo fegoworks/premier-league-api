382cc785d69e3cefcaf01f9bfe7278f0c149c4a9
unknown <fegoworks@hotmail.com>
2020-01-31 15:32:09 +0100

    feat(edit-fixtures): create controller logic for edtitng fixtures

--- src/controllers/fixture.controller.js
+++ src/controllers/fixture.controller.js
@@ -1,4 +1,5 @@
-import FixtureModel from '../models/fixture.model'
+import FixtureModel from '../models/fixture.model';
+import TeamModel from '../models/team.model';
 
 class FixtureController {
   /**
@@ -8,17 +9,16 @@ class FixtureController {
    * @returns {object} team object
    */
   static async addFixture(req, res) {
-    const user = req.id
-    const fixture = req.body
+    const user = req.id;
+    const fixture = req.body;
     try {
-
-      const createdFixture = await FixtureModel.add(fixture, user)
+      const createdFixture = await FixtureModel.add(fixture, user);
 
       return res.status(200).json({
-        status: "success",
+        status: 'success',
         message: `"${createdFixture.homeTeam} vs ${createdFixture.awayTeam}" created successfully`,
-        data: createdFixture
-      })
+        data: createdFixture,
+      });
     } catch (error) {
       if (error.code === '23505') {
         return res.status(409).json({
@@ -32,6 +32,51 @@ class FixtureController {
       });
     }
   }
+
+  static async editFixture(req, res) {
+    const fixtureId = req.params.id;
+    const userId = req.id;
+    const fixture = req.body;
+    try {
+      const args = {
+        fixtureId,
+        userId,
+        fixture
+      };
+      const found = await FixtureModel.findById(fixtureId)
+
+      if (found) {
+        const updatedFixture = await FixtureModel.edit(args);
+
+        return res.status(200).json({
+          status: 'success',
+          message: `Fixtures updated successfully`,
+          data: updatedFixture,
+        });
+      }
+      return res.status(400).json({
+        status: 'Request failed',
+        error: 'Fixture not found',
+      })
+
+    } catch (error) {
+      if (error.code === '23505') {
+        return res.status(409).json({
+          status: 'Request failed',
+          error: 'This fixture already exists',
+        });
+      } else if (error.code === '23503') {
+        return res.status(409).json({
+          status: 'Request failed',
+          error: 'Wrong team credentials provided',
+        });
+      }
+      return res.status(400).json({
+        status: 'Request failed',
+        error: error,
+      });
+    }
+  }
 }
 
-export default FixtureController
+export default FixtureController;
